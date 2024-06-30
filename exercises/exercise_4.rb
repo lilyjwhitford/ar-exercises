3d69777d0eebd9df530811c33a697bc01bfccdd7
Lily Whitford <lilywhitford2@gmail.com>
2024-06-30 03:15:30 +0000

    load womens clothing stores than generate < 1M in revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -14,11 +14,17 @@ Whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel:
 Yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 # use where class method to fetch stores that carry mens apparel
-@mens_stores = Store.where(mens_apparel: true)
-
 # loop through stores and output the name and annual revenue
+@mens_stores = Store.where(mens_apparel: true)
 @mens_stores.each do |store|
   puts store.name
   puts store.annual_revenue
 end
+
+# loop through stores that carry womens apparel AND generating less than 1M in yearly rev
+@women_low_revenue_stores = Store.where(womens_apparel: true, annual_revenue: { lt: 1000000 })
+@women_low_revenue_stores.each do |store|
+  puts "#{store.name} - #{store.annual_revenue}"
+end
+
 

635114888d2787fb01b4df44633783b2f7b33785
Lily Whitford <lilywhitford2@gmail.com>
2024-06-30 03:24:59 +0000

    add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# add validations
+
+validates :name, length: { minimum: 3 }, presence: true
+validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+validate :must_carry_apparel
+
+def must_carry_apparel
+  errors.add(:base, 'Must carry at least one type of apparel') unless mens_apparel || womens_apparel
+end

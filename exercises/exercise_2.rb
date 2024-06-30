226fce55a3125a3ecd3aaf6d33d491bc309b1b5f
Lily Whitford <lilywhitford2@gmail.com>
2024-06-30 03:02:16 +0000

    update store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# load first and second store and assign it to @store1 & @store2
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# update first store to new name
+@store1.update(name: "Brentwood")
+
+# save to database
+@store1.save

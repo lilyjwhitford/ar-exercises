10b0ce4bc4aadac79af6e7b591e1f6658769822b
Lily Whitford <lilywhitford2@gmail.com>
2024-06-30 03:23:05 +0000

    create relationship in AR for employees and stores tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# add some fake data into employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Lily", last_name: "Whitford", hourly_rate: 45)
